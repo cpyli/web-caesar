a30893fe6937ba41360f7c1632c6fbaa0d2e74d1
cpyli <cpyli@uw.edu>
2017-06-11 15:43:28 -0700

    Created Web Caesar app

---
+++ main.py
@@ -0,0 +1,57 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+
+        </style>
+    </head>
+
+    <body>
+        <form id="form" method="POST" action="/encrypt">
+            <label>Rotate by:</label>
+                <input name="rot" type="text" />
+                <textarea id="textarea" name="textarea"> {0} </textarea>
+                <input type="submit" />
+        </form>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+
+    return form.format("")
+
+@app.route("/encrypt", methods=["POST"])
+def encrypt():
+    rot_request = request.form["rot"]
+    rot = int(rot_request)
+    text_request = request.form["textarea"]
+    text = rotate_string(text_request, rot)
+
+    return "<h1>" + form.format(text) + "</h1>"
+
+app.run()
